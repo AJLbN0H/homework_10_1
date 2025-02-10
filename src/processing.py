f80c48a017cd147dc491ab74ee2968a7a09d4152
alexander vagin <sashavagin1987@gmail.com>
2025-02-10 18:46:16 +1000

    the function filter_by_state works correctly

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,12 @@
 from typing import Iterable
 
 def filter_by_state(user_dict: list[dict], state:str='EXECUTED'):
-    return user_dict
+    """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует указанному значению."""
+    dict_executed = []
+    for element in user_dict:
+        if element['state'] == state:
+            dict_executed.append(element)
+    return dict_executed
 
 
 print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))

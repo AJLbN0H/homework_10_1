5be3f4085881ff92d16ac30a2da7bbb3f03a5d23
alexander vagin <sashavagin1987@gmail.com>
2025-02-25 17:43:35 +1000

    Выполнены тесты функции sort_by_date по пункту 6 домашнего задания

--- src/processing.py
+++ src/processing.py
@@ -24,12 +24,24 @@ def filter_by_state(
         return 'Транзакции отсутствуют'
 
 
-print(filter_by_state(user_transaction_list))
+#print(filter_by_state(user_transaction_list))
+
 
 
 def sort_by_date(user_dict: Iterable[dict], reverse: bool = True) -> Iterable[dict]:
     """Функция возвращает список отсортированный по дате"""
-    return sorted(user_dict, key=lambda x: x["date"], reverse=reverse)
+    if user_dict is None or user_dict == '':
+        return 'Вы не ввели данные по транзакциям'
+    else:
+        seen_dates = set()
+        unique_transaction = []
+
+        for transaction in user_dict:
+            if transaction['date'] not in seen_dates:
+                seen_dates.add(transaction['date'])
+                unique_transaction.append(transaction)
+
+        return sorted(unique_transaction, key=lambda x: x["date"], reverse=reverse)
 
 
-#print(sort_by_date(user_transaction_list))
+print(sort_by_date(user_transaction_list))

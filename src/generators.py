c00a1651b104ca160a32f09d8d1dc26958d14941
alexander vagin <sashavagin1987@gmail.com>
2025-03-02 00:02:57 +1000

    Тестирование функции filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -1,84 +1,5 @@
 from typing import Iterable, Union
 
-input_user_transaction = (
-    [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }
-    ]
-)
 
 def filter_by_currency(user_transaction: Iterable[dict], currency: Union[str]) -> iter:
     """Функция-генератор возвращающая итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной"""
@@ -86,9 +7,6 @@ def filter_by_currency(user_transaction: Iterable[dict], currency: Union[str]) -
         if transaction['operationAmount']['currency']['code'] == currency:
             yield transaction
 
-usd_transaction = filter_by_currency(input_user_transaction, "USD")
-for _ in range(3):
-    print(next(usd_transaction))
 
 
 def transaction_descriptions(user_transaction: Iterable[dict]) -> iter:
@@ -96,9 +14,8 @@ def transaction_descriptions(user_transaction: Iterable[dict]) -> iter:
     for transaction in user_transaction:
         yield transaction["description"]
 
-descriptions = transaction_descriptions(input_user_transaction)
-for _ in range(5):
-    print(next(descriptions))
+#descriptions = transaction_descriptions(input_user_transaction)
+#print(next(descriptions))
 
 
 def card_number_generator(start: Union[int], stop: Union[int]) -> str:
@@ -110,5 +27,5 @@ def card_number_generator(start: Union[int], stop: Union[int]) -> str:
         part_4 = num % 10000
         yield f"{part_1:04} {part_2:04} {part_3:04} {part_4:04}"
 
-for card_number in card_number_generator(1, 5):
-    print(card_number)
+#for card_number in card_number_generator(1, 5):
+    #print(card_number)
